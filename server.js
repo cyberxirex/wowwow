1b34f790d3f3c1d368383562ac18616817b690e8
Hyungseob Won <stepanowon@hotmail.com>
2024-12-21 14:40:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const os = require('os');
 // 
-const PORT = 80;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // 주석 추가
@@ -11,8 +11,8 @@ const app = express();
 app.get('/', (req, res) => {
   return res.status(200).send(`
   <div>
-    <h2> Version : 1.0.0 </h2>
-    <h2> 호스트명 :: ${os.hostname()} </h2>
+    <h2> Version : 1.0.1 </h2>
+    <h2> 호스트명 : ${os.hostname()} </h2>
   </div>
   `);
 });
